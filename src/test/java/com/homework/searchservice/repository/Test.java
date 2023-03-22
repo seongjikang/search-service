107952cce01c922abe5402f3243624a847207ec2
seongjikang <java7ang@gmail.com>
2023-03-22 23:02:59 +0900

    테스트 명칭 변경

--- src/test/java/com/homework/searchservice/repository/Test.java
+++ src/test/java/com/homework/searchservice/repository/Test.java
@@ -3,8 +3,6 @@ package com.homework.searchservice.repository;
 import com.homework.searchservice.entity.Search;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,13 +12,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
-public class RepositoryTest {
+public class Test {
     @Autowired
     SearchRepository searchRepository;
 
@@ -56,7 +51,7 @@ public class RepositoryTest {
             searchRepository.increaseSearchCnt("data1");
     }
 
-    @Test
+    @org.junit.Test
     public void 검색기록_증가_및_조회_테스트() {
 
         for(int i =1; i <= 10 ; i ++) {
@@ -67,7 +62,7 @@ public class RepositoryTest {
 
     }
 
-    @Test
+    @org.junit.Test
     public void 검색기록_탑10_조회_테스트() {
         Sort sort = Sort.by(Sort.Direction.DESC, "searchCnt");
         Pageable pageable = PageRequest.of(0,10, sort);
@@ -79,7 +74,7 @@ public class RepositoryTest {
 
     }
 
-    @Test
+    @org.junit.Test
     public void 검색기록_증가_테스트() {
         Search search = Search.SearchBuilder()
                 .searchCnt(1L)
